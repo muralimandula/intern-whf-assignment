c381e5be4a893afae3bed853cfd0c4426398d839
muralimandula <murali.mandula@msitprogram.net>
2020-05-29 15:23:10 +0530

    Rest template auto-wired, added conversion rate method(No Mapping).

--- ezepay/src/main/java/com/example/ezepay/services/CurrencyConversionService.java
+++ ezepay/src/main/java/com/example/ezepay/services/CurrencyConversionService.java
@@ -2,6 +2,7 @@ package com.example.ezepay.services;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,30 +12,41 @@ import com.example.ezepay.models.CurrencyExchangeRateResponse;
 import com.example.ezepay.models.Rates;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
+@SuppressWarnings("unchecked")
 @RestController
 public class CurrencyConversionService {
+	
+	@Autowired
+	RestTemplate restTemplate;
 	
 	private static String exchangeRateUrl = "https://open.exchangerate-api.com/v6/latest";
 	private Rates rates;
 	
-	@GetMapping("/currencyRates")
+	@GetMapping("/conversionRates")
 	public Rates getAllConversionRates() {
 		
 		if(this.rates == null) {
-			RestTemplate restTemplate = new RestTemplate();
 			CurrencyExchangeRateResponse currencyExchangeRateResponse = restTemplate.getForObject(exchangeRateUrl, CurrencyExchangeRateResponse.class);
 			this.rates = currencyExchangeRateResponse.getRates(); 
 		}
 		return this.rates;
 	}
 
-	@SuppressWarnings("unchecked")
-	@GetMapping("/currencyRates/{currency}")
-	public Double getConversionRate(@PathVariable String currency) {
+	@GetMapping("/conversionRates/{currency}")
+	public Double getConversionRateEndPoint(@PathVariable String currency) {
 
 		if(this.rates == null) {
-			RestTemplate restTemplate = new RestTemplate();
+			CurrencyExchangeRateResponse currencyExchangeRateResponse = restTemplate.getForObject(exchangeRateUrl, CurrencyExchangeRateResponse.class);
+			this.rates = currencyExchangeRateResponse.getRates(); 
+		}
+		Map<String, Object> rateConversionMap = new ObjectMapper().convertValue(this.rates, Map.class);
+		
+		return Double.valueOf(rateConversionMap.get(currency.toUpperCase()).toString());
+	}
+	
+	public Double getConversionRate(String currency) {
+
+		if(this.rates == null) {
 			CurrencyExchangeRateResponse currencyExchangeRateResponse = restTemplate.getForObject(exchangeRateUrl, CurrencyExchangeRateResponse.class);
 			this.rates = currencyExchangeRateResponse.getRates(); 
 		}
